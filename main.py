0eb1ddff30a07167d9ac5bd2b15aaa7f99c1578f
Coder Phantom <codephantom71@gmail.com>
2025-11-23 19:28:27 +0100

    Created add_task function and binded with add button

--- main.py
+++ main.py
@@ -1,44 +1,55 @@
+# Import tkinter 
 from tkinter import *
+from tkinter import font
 
+# Add task function
+def add_task(event, paned_W, fenetre):
+    print("Add boutton")
+    paned_W.add(Checkbutton(fenetre, text='test', background= "black"))
+
+# Get the position of the mouse
+def get_position(event):
+    print(event.x, event.y)
+
+#Main function 
 def main():
-# Création du fenêtre 
+# Creation of the window 
     fenetre = Tk()
     fenetre.title("ToDo")
     fenetre.geometry("800x600")
     fenetre.configure(bg = "ivory")
 
 
-    #keys configure
-    # print(fenetre.configure().keys())
-
+    # Window configure
     fenetre.rowconfigure(1, weight =1)
     fenetre.columnconfigure(0, weight =1)
 
+    # Creation of add Button
+    bouton = Button(fenetre, text = "Add", width= 10, height=3 )
 
-    bouton = Button(fenetre, text = "Add")
-    label = Label(fenetre, text = "Hello World", bg='black', )
     
 
-    # canvas = Canvas(fenetre, width= 850 , height = 850, background= 'ivory')
-    # canvas.grid(row = 1, column = 3, pady = 6)
-
-    # liste de case à cocher dans laquelle un seul élément peut être choisi 
-    # bouton2 = tkinter.Radiobutton(fenetre, text="Non", variable=tkinter.StringVar(), value=2)
-    # bouton2.pack()
-
     # bar d'écriture 
-    bar_write = Entry(fenetre, font=("Calibiri 10") )
-    bar_write
+    bar_write = Entry(fenetre, font=("mononoki 30"), border= 1, highlightthickness = 2, highlightbackground ="#262626", )
+    
+    # PanedWindow
+    p = PanedWindow(fenetre, orient='vertical')
+    p.add(Label(p, text = "Essaie"))
 
-    # print(Entry().keys())
 
-    bar_write.grid(row = 0, column = 0, sticky="ew", padx = 10, pady= 20)
-    bouton.grid(row = 0, column = 3, padx = 5)
-    # label.grid()
+    
+    # Add widgets
+    bar_write.grid(row = 0, column = 0, sticky="ew", padx = 5, pady= 10)
+    bouton.grid(row = 0, column = 1, padx = 10)
+    p.grid(row = 1, column=0,columnspan = 2, sticky= 'ewns', padx = 5)
 
+    # Connect function with 'add buton'
+    fenetre.bind("<Button-1>", lambda event: add_task(event, p, fenetre))
 
 
     # Loop
     fenetre.mainloop()
 
+
+#Usin main function
 main()
